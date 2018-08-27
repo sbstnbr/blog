becc1ab56a53fee875179370e91b08c78a96d49a
Sébastien BRO <sebastien.bro.c@thalesdigital.io>
2018-08-27 18:53:21 +0200

    feat(api): get posts

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 
 
 # Instagram scrapping
-https://api.instagram.com/v1/users/self/media/recent/?access_token=
--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 require('dotenv').config()
+const axios = require("axios");
 
 console.log(process.env.INSTAGRAM_ACCESS_TOKEN);
+
+const getPosts = async () => {
+    try {
+        const response = await axios.get(
+            "https://api.instagram.com/v1/users/self/media/recent/?access_token="
+            +process.env.INSTAGRAM_ACCESS_TOKEN
+            +"&count=33"
+        );
+        return response;
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+getPosts()
+    .then(posts => {
+        console.log(posts.data.data);
+        console.log(posts.data.data.length);
+    });
